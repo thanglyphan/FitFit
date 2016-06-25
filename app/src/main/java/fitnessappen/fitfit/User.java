f88ab3ad6c4b7215baf1471dcc132e28dc1f50c2
thanglyphan <lyern52@gmail.com>
2016-06-25 12:41:30 +0200

    Add user

---
+++ app/src/main/java/fitnessappen/fitfit/User.java
@@ -0,0 +1,15 @@
+package fitnessappen.fitfit;
+
+/**
+ * Created by Thang on 25.06.2016.
+ */
+public class User
+{
+    public String Username;
+    public String Password;
+
+    public User(String a, String b){
+        this.Username = a;
+        this.Password = b;
+    }
+}
