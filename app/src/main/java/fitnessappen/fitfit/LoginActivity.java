0fe8ecdc52f8360a5d4fd1607d6cf5e7d8a9bd42
thanglyphan <lyern52@gmail.com>
2016-06-25 12:40:59 +0200

    Add user

--- app/src/main/java/fitnessappen/fitfit/LoginActivity.java
+++ app/src/main/java/fitnessappen/fitfit/LoginActivity.java
@@ -1,13 +1,28 @@
 package fitnessappen.fitfit;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.microsoft.windowsazure.mobileservices.*;
+import com.microsoft.windowsazure.mobileservices.http.OkHttpClientFactory;
+import com.microsoft.windowsazure.mobileservices.http.ServiceFilterResponse;
+import com.microsoft.windowsazure.mobileservices.table.MobileServiceTable;
+import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
 
 public class LoginActivity extends AppCompatActivity {
+    private MobileServiceClient mClient;
+
+    private MobileServiceTable<User> mUserTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
 }
